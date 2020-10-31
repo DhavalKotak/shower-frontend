6a782e7c7bf25e338ca904197bea4bd0695a66e5
DhavalKotak <DhavalKotak@users.noreply.github.com>
2020-10-31 20:02:59 +0530

    Updates

---
+++ precache-manifest.18e7575fe7661322f5658e52e6fa643c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "203a84b31078f085f89cd61888edac9f",
+    "url": "/shower-frontend/index.html"
+  },
+  {
+    "revision": "0337222a6f59a60a5c46",
+    "url": "/shower-frontend/static/css/main.fa233907.chunk.css"
+  },
+  {
+    "revision": "ebf8c4c36de2c81b1008",
+    "url": "/shower-frontend/static/js/2.9272c4fe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shower-frontend/static/js/2.9272c4fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0337222a6f59a60a5c46",
+    "url": "/shower-frontend/static/js/main.7fba7fbe.chunk.js"
+  },
+  {
+    "revision": "b0f4a21e7ce605f52343",
+    "url": "/shower-frontend/static/js/runtime-main.e5f2ba30.js"
+  },
+  {
+    "revision": "e6b145c40cabd25a6b957fd658a8305a",
+    "url": "/shower-frontend/static/media/music-note.e6b145c4.png"
+  },
+  {
+    "revision": "a13cc0c599455d2838c688369442ab20",
+    "url": "/shower-frontend/static/media/shower.a13cc0c5.png"
+  }
+]);
